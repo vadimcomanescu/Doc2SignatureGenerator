0459fb90d5fdf077b912f64b72a05b99bacd2298
Vadim Comanescu <vadim984@gmail.com>
2012-08-03 12:36:00 +0300

    Properly generate signatures for classes that have their methods used in procedural style also

--- Doc2SignatureGenerator.php
+++ Doc2SignatureGenerator.php
@@ -32,14 +32,14 @@ class Doc2SignatureGenerator {
 	}
 	
 	private static function parseMethodParamsFrom($line) {
-		if (preg_match('#class="methodparam">void</span>#', $line)) return null;
+		if (preg_match('#class="methodparam">void</span>#', $line)) return array('paramVariable' => null, 'optional' => false);
 		preg_match('#<code class=("parameter"|"parameter reference")>(\S+)</code>#', $line, $matches);
 		$isOptionalParam = preg_match('/\[/', $line) ? true : false;
 		return array('paramVariable' => $matches[2], 'optional' => $isOptionalParam);
 	}
 	
 	private static function parseClassNameFrom($line) {
-		preg_match('/class="classname">(\S+)</', $line, $matches);
+		preg_match('/class="classname">(\S+)</U', $line, $matches);
 		return $matches[1];
 	}
 	
@@ -50,7 +50,7 @@ class Doc2SignatureGenerator {
 			if (self::lineDefinesMethodName($line)) $methodName = self::parseMethodNameFrom($line);
 			if (self::lineDefinesMethodParam($line)) {
 				if (!$methodName) continue;
-				$signatureData[$methodName][] = self::parseMethodParamsFrom($line);		
+				$signatureData['methods'][$methodName][] = self::parseMethodParamsFrom($line);		
 			}
 		}
 		return $signatureData;
@@ -58,24 +58,40 @@ class Doc2SignatureGenerator {
 	
 	private static function stringifySignatures($signatures) {
 		$result = "<?php\n";
-		foreach ($signatures as $signatureData) 
-			$result .= self::stringifySignatureData($signatureData);	
+		$signatures = self::splitSignaturesInOOAndProcedural($signatures);
+		foreach ($signatures['oo'] as $className => $data) 
+			$result .= self::stringifySignatureData(array('classname' => $className, 'methods' => $data['methods']));
+		$result .= self::stringifySignatureData($signatures['procedural']);	
 		return $result;
+	}
+	
+	private static function splitSignaturesInOOAndProcedural($signatures) {
+		$sigs = array();
+		foreach ($signatures as $signatureData) {
+			if (!isset($signatureData['classname'])) {
+				if (!isset($sigs['procedural'])) $sigs['procedural'] = array();
+				$sigs['procedural'] = array_merge($sigs['procedural'], $signatureData);
+			} else {
+				if (!isset($sigs['oo'][$signatureData['classname']]['methods'])) $sigs['oo'][$signatureData['classname']]['methods'] = array();
+				$sigs['oo'][$signatureData['classname']]['methods'] = array_merge($sigs['oo'][$signatureData['classname']]['methods'], $signatureData['methods']);
+			}
+		}
+		return $sigs;
 	}
 	
 	private static function stringifySignatureData($signatureData) {
 		if (isset($signatureData['classname']))  {
 			$classname = $signatureData['classname'];
 			unset($signatureData['classname']);
-			return sprintf("class %s {\n%s}\n", $classname, self::stringifyMethodWithParams($signatureData, true));
+			return sprintf("class %s {\n%s}\n", $classname, self::stringifyMethodWithParams($signatureData['methods'], $classname));
 		}
-		return self::stringifyMethodWithParams($signatureData, false);
+		return self::stringifyMethodWithParams($signatureData['methods']);
 	}
 	
-	private static function stringifyMethodWithParams($methodWithParams, $forClass = false) {
+	private static function stringifyMethodWithParams($methodWithParams, $className = null) {
 		$result = '';
 		foreach ($methodWithParams as $methodName => $paramData) {
-			$result .= sprintf("%sfunction %s(%s);\n", $forClass ? '    ' : '', $methodName, implode(", ", array_map(function ($e) {
+			$result .= sprintf("%sfunction %s(%s);\n", $className ? '    ' : '', str_ireplace($className.'_', '', $methodName), implode(", ", array_map(function ($e) {
 				return $e['optional'] ? $e['paramVariable'] . ' = null' : $e['paramVariable'];
 			}, $paramData)));
 		}
